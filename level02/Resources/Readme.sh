cd2075dd0b65cb4e1316d6d81376b1f60e1f83bb
kysgramo <kysgramo@student.21-school.ru>
2021-12-05 15:03:06 +0300

    g

--- level02/Resources/Readme.sh
+++
@@ -1,3 +0,0 @@
-#https://ru.wikipedia.org/wiki/Pcap
-scp -P 5000 scp://level02@192.168.24.113/level02.pcap /Users/kysgramo/snow-crash/level02/Resources
-chmod 777 level02.pcap 
---
+++ level09/Resources/break.py
@@ -0,0 +1,5 @@
+import sys
+
+for i, val in enumerate(sys.argv[1]):
+	sys.stdout.write(chr(ord(val)-i))
+	#sys.stdout.flush()
